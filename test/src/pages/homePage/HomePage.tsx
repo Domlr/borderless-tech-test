725341c63ab3e58878165aee27fda0f5af3302b9
dominic Roof <dominiclroof@gmail.com>
2024-02-24 00:37:15 +0000

    add in stuff

--- test/src/pages/homePage/HomePage.tsx
+++ test/src/pages/homePage/HomePage.tsx
@@ -24,7 +24,7 @@ const UsersPage: React.FC = () => {
     <div className="container mx-auto p-4">
       <TitleComponent />
 
-      <div className="flex flex-col w-full sm:flex-row justify-end items-center px-4 py-2 bg-white shadow">
+      <div className="flex flex-col w-full sm:flex-row justify-end items-center py-2 ">
         <input
           type="text"
           placeholder="Search by name..."
@@ -48,15 +48,21 @@ const UsersPage: React.FC = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {filteredUsers.map((user) => (
-          <Link
-            key={user.id}
-            to={`/user/${user.id}`}
-            className="block p-4 border border-gray-200 rounded shadow hover:shadow-md transition-shadow"
-          >
-            <CardComponent user={user} />
-          </Link>
-        ))}
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => (
+            <Link
+              key={user.id}
+              to={`/user/${user.id}`}
+              className="block p-4 border border-gray-200 rounded shadow hover:shadow-md transition-shadow"
+            >
+              <CardComponent user={user} />
+            </Link>
+          ))
+        ) : (
+          <div className="flex justify-center w-full">
+            There are no users matching your criteria.
+          </div>
+        )}
       </div>
     </div>
   );
